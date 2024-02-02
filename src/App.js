7feb0a4f4e875738f3c484650ab8ad45af42a529
Kaushik Verma <kaushikverma321@gmail.com>
2024-02-02 11:14:41 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import vega from 'vega';
+import vegaLite from 'vega-lite';
+import vl from 'vega-lite-api';
+import { Handler } from 'vega-tooltip';
+import { config } from './config';
+import { getData } from './getData';
+import { viz } from './viz';
+
 import './App.css';
 
 function App() {
+
+  vl.register(vega, vegaLite, {
+    view: { renderer: 'svg' },
+    init: view => { view.tooltip(new Handler().call); }
+  });
+
+  const run = async () => {
+    const marks = viz
+      .data(await getData())
+      .width(window.innerWidth)
+      .height(window.innerHeight)
+      .autosize({ type: 'fit', contains: 'padding' })
+      .config(config);
+
+    document.body.appendChild(await marks.render());
+  };
+
+  run();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
